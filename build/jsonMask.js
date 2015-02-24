20b3c8bbcf2164f5b34d690889eaf4f1d4e3dc29
Yuriy Nemtsov <nemtsov@gmail.com>
2015-02-23 23:35:03 -0500

    create front-end build

--- build/jsonMask.js
+++ build/jsonMask.js
@@ -1,5 +1,5 @@
 /**
- * json-mask | (c) 2014 Yuriy Nemtsov | https://github.com/nemtsov/json-mask/blob/master/LICENSE
+ * json-mask | (c) 2015 Yuriy Nemtsov | https://github.com/nemtsov/json-mask/blob/master/LICENSE
  * @license
  */
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.jsonMask=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
@@ -153,7 +153,7 @@ function _forAll(obj, mask, fn) {
   for (key in obj) {
     if (!util.has(obj, key)) continue
     value = fn(obj, key, mask)
-    if (value) ret[key] = value
+    if ('undefined' !== typeof value) ret[key] = value
   }
   return ret
 }
