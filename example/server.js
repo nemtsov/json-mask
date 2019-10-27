357183cd686daacf26697e2e5a99f7ecac26568c
Yuriy Nemtsov <nemtsov@gmail.com>
2019-10-27 18:34:40 -0400

    package: upgrade all dev-dependencies; standard style fixes

--- example/server.js
+++ example/server.js
@@ -1,9 +1,9 @@
 var http = require('http')
-  , url = require('url')
-  , mask = require('../lib')
-  , activities = require('../test/fixture/activities.json')
-  , personalActivities = require('../test/fixture/personal_activities.json')
-  , server
+var url = require('url')
+var mask = require('../lib')
+var activities = require('../test/fixture/activities.json')
+var personalActivities = require('../test/fixture/personal_activities.json')
+var server
 
 /**
  * Using json-mask to implement the Google API Partial Responses
@@ -12,15 +12,15 @@ var http = require('http')
 
 server = http.createServer(function (req, res) {
   var parsedUrl = url.parse(req.url, true)
-    , data = /^\/personal/.test(parsedUrl.pathname) ? personalActivities : activities
-    , query = parsedUrl.query
-  res.writeHead(200, {'Content-Type': 'application/json'})
+  var data = /^\/personal/.test(parsedUrl.pathname) ? personalActivities : activities
+  var query = parsedUrl.query
+  res.writeHead(200, { 'Content-Type': 'application/json' })
   res.end(JSON.stringify(mask(data, query.fields), true, 2))
 })
 
 server.listen(4000, function () {
   var prefix = 'curl \'http://localhost:4000%s?fields=%s\''
-  console.log('Server runnong on :4000, try the following:');
+  console.log('Server runnong on :4000, try the following:')
   console.log(prefix, '/', 'title')
   console.log(prefix, '/', 'kind,updated')
   console.log(prefix, '/', 'url,object(content,attachments/url)')
--- test/browser/test.js
+++ test/browser/test.js
@@ -1,7 +1,7 @@
 /* global jsonMask */
 
 function assert (o) { if (!o) throw new Error('AssertionError') }
-var r = jsonMask({p: {a: 1, b: 2}, z: 1}, 'p/a,z')
+var r = jsonMask({ p: { a: 1, b: 2 }, z: 1 }, 'p/a,z')
 assert(r.p.a)
 assert(r.z)
 assert(typeof r.p.b === 'undefined')
